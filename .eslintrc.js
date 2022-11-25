fb3719e5c19d8ebd57987b04a45a10dcd8bc7110
Iain Cunningham <iaincunningham@me.com>
2022-11-25 07:22:59 -0800

    Introduce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
--- src/getPatent.ts
+++ src/getPatent.ts
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 import { fetchCaseId } from './helpers/fetchCaseId'
 import { searchPatents } from './searchPatents'
 import { getPatentByGuid } from './getPatentByGuid'
@@ -7,21 +5,19 @@ import { getPatentByGuid } from './getPatentByGuid'
 import { PPubsPatentHighlightAPIResponse } from 'src/types'
 
 /**
- *  Fetch a single patent from the USPTO Patent Public Search: https://ppubs.uspto.gov/pubwebapp/ 
+ *  Fetch a single patent from the USPTO Patent Public Search: https://ppubs.uspto.gov/pubwebapp/
  *  @param {string} patentNum - the patent number to fetch
  *  @returns {Promise} - resolves to PPubsPatentHighlightAPIResponse type, or
  *      rejects promise if bad patent number or error on retrieval
  */
-export async function getPatent(patentNum: string): Promise<PPubsPatentHighlightAPIResponse> {
-    const caseId = await fetchCaseId()
+export async function getPatent (patentNum: string): Promise<PPubsPatentHighlightAPIResponse> {
+  const caseId = await fetchCaseId()
 
-    const searchQuery = patentNum + '.pn.'
-    const searchResults = await searchPatents(searchQuery, caseId)
-    if( !searchResults ) { throw new Error('Could not retrieve search results from ppubs search API.') }
+  const searchQuery = patentNum + '.pn.'
+  const searchResults = await searchPatents(searchQuery, caseId)
 
-    const patentGuid = searchResults.patents[0].guid
-    const patentResult = await getPatentByGuid(patentGuid)
-    if( !patentResult ) { throw new Error('Could not retrieve patent results from ppubs highlight API.') }
+  const patentGuid = searchResults.patents[0].guid
+  const patentResult = await getPatentByGuid(patentGuid)
 
-    return patentResult
+  return patentResult
 }
