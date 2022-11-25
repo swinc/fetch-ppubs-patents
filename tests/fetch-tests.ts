67ef1275c59f5cbe3bca979f7f788467cda90b73
Iain Cunningham <iaincunningham@me.com>
2022-11-25 10:28:32 -0800

    Add searchResults() test

--- tests/fetch-tests.ts
+++ tests/fetch-tests.ts
@@ -12,12 +12,21 @@ describe('fetch tests', function () {
   const patentNumber = '8139878'
   const patentNumbers = ['8139878', '11276648', '11275662']
 
-  it('should correctly fetch ppub session case id', async function () {
+  it('should correctly fetch a case id', async function () {
     const caseId = await fetchCaseId()
     expect(caseId).to.be.a('number')
   })
 
-  it('should correctly fetch patent data with a guid', async function () {
+  it('should correctly return search results', async function () {
+    const query = patentNumbers.map((num) => num + '.pn.').join(' OR ')
+    const searchResults = await fp.searchPatents(query)
+    expect(searchResults.numFound).to.equal(3)
+    expect(searchResults.patents[0].guid).to.equal('US-11276648-B2')
+    expect(searchResults.patents[1].guid).to.equal('US-11275662-B2')
+    expect(searchResults.patents[2].guid).to.equal('US-8139878-B2')
+  })
+
+  it('should correctly fetch patent by guid', async function () {
     const guid = 'US-8139878-B2'
     const queryResult = await fp.getPatentByGuid(guid)
     expect(queryResult).to.be.an('object')
